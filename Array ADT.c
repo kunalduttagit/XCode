1a9aa6dd185edecb4507b6e5d76ae2cd18f00d4f
Kunal Dutta <kunaldutta0305@gmail.com>
2022-10-07 19:25:51 +0530

    Update 2.0 - Added Extra Operations - Fixed Bugs in Recursive Binary Search - Fixed Case 6 automatically jumping to case 7 (another break statement after all nested switch case) - Added few Time Complexity Comments - Program now exits at Code 8 instead of 11 - Some Schematic Changes

--- Array ADT.c
+++ Array ADT.c
@@ -119,10 +119,10 @@ int MoveToHeadLinearSearch (struct Array *arr, int key)
     return -1;
 }
 
-int BinarySearch(struct Array arr, int key)
-{
+int BinarySearch(struct Array arr, int key) //best O(1) Worst O(log n) same for successful/unsuccesful
+{                                           //ends at last nodes of tree 1 node 2 dead ends - unsucces
     int low, mid, high;
-    low = 0; high = arr.length-1;
+    low = 0; high = arr.length-1;           //exactly O(log(n+1))
     while(low <= high)
     {
         mid = (low+high)/2;
@@ -136,21 +136,79 @@ int BinarySearch(struct Array arr, int key)
     return -1;
 }
 
-int RecursiveBinarySearch(int a[], int low, int high, int key)
+int RecursiveBinarySearch(int a[], int low, int high, int key) //best O(1) Worst O(log n) same for successful/unsuccesful
 {
     int mid;
-    while(low <= high)
+    if(low <= high)
     {
         mid = (low+high)/2;
         if(key == a[mid])
             return mid;
         else if(key < a[mid])
-            RecursiveBinarySearch(a, low, mid-1, key);
+            return RecursiveBinarySearch(a, low, mid-1, key);
         else
-            RecursiveBinarySearch(a, mid+1, high, key);
+            return RecursiveBinarySearch(a, mid+1, high, key);
     }
     return -1;
 }
+
+int Get(struct Array arr, int index)
+{
+    if(index >= 0 && index<arr.length)
+        return arr.A[index];
+    return -1;
+}
+
+void Set(struct Array *arr, int index, int val)
+{
+    if(index >= 0 && index < arr->length)
+        arr->A[index] = val;
+    printf("After change ");
+    Display(*arr);
+}
+
+int Max(struct Array arr)
+{
+    int max = arr.A[0];
+    for(int i=0; i<arr.length; i++)
+    {
+        if(arr.A[i] > max)
+            max = arr.A[i];
+    }
+    return max;
+}
+
+int Min(struct Array arr)
+{
+    int min = arr.A[0];
+    for(int i=0; i<arr.length; i++)
+    {
+        if(arr.A[i] < min)
+            min = arr.A[i];
+    }
+    return min;
+}
+
+int RecursiveSum(int a[], int n)
+{
+    if(n < 0)
+        return 0;
+    return RecursiveSum(a, n-1) + a[n];
+}
+
+int Sum(struct Array arr)
+{
+    int total=0;
+    for(int i=0; i<arr.length; i++)
+        total += arr.A[i];
+    return total;
+}
+
+float Average(struct Array arr)
+{
+    return (float)Sum(arr)/arr.length ;
+}
+
 
 int main()
 {
@@ -164,9 +222,10 @@ int main()
     printf("\n4. Append element");
     printf("\n5. Printing the elements of array");
     printf("\n6. Searching");
-    printf("\n11. Exit\n");
+    printf("\n7. Arithmetic Operations");
+    printf("\n8. Exit\n");
     
-    while(opt !=11)
+    while(opt !=8)
     {
         printf("\nEnter the sequence of operation\n");
         scanf("%d", &opt);
@@ -210,7 +269,7 @@ int main()
             case 6:
             {
                 printf("Choose Search Type ");
-                printf("\n1. Linear Search\n2. Transposition LS\n3. Move to Head LS\n4. Binary Search\n5.Recursive Binary Search\n(Note: 2 and 3 will affect your array\n");
+                printf("\n1. Linear Search\n2. Transposition LS\n3. Move to Head LS\n4. Binary Search\n5. Recursive Binary Search\n(Note: 2 and 3 might affect your array\n");
                 int s; scanf("%d", &s);
                 switch(s)
                 {
@@ -259,6 +318,56 @@ int main()
                     default: break;
                         
                 }
+                break;
+            case 7:
+                {
+                    printf("\n1. Get Element by Index\n2. Change value at some Index\n3. Find Maximum\n4. Find Minimum\n5. Find Sum of all Elements\n6. Find Average of all Elements\n7. Recursive Sum\n");
+                    int ao; scanf("%d", &ao);
+                    switch(ao)
+                    {
+                        case 1:
+                        {
+                            printf("\nEnter Index for the element ");
+                            int x; scanf("%d", &x);
+                            x != -1 ? printf("The Element at %d is %d", x, Get(arr, x)) : printf("Element Not Found!");
+                            break;
+                        }
+                        case 2:
+                        {
+                            printf("\nEnter Index and the new value ");
+                            int index, val; scanf("%d %d", &index, &val);
+                            Set(&arr, index, val);
+                            break;
+                        }
+                        case 3:
+                        {
+                            printf("\nMaximum is %d", Max(arr));
+                            break;
+                        }
+                        case 4:
+                        {
+                            printf("\nMinimum is %d", Min(arr));
+                            break;
+                        }
+                        case 5:
+                        {
+                            printf("\nSum is %d", Sum(arr));
+                            break;
+                        }
+                        case 6:
+                        {
+                            printf("\nAverage is %f", Average(arr));
+                            break;
+                        }
+                        case 7:
+                        {
+                            printf("\nSum using Reccursion is %d", RecursiveSum(arr.A, arr.length));
+                            break;
+                        }
+                        default: break;
+                            
+                    }
+                }
             }
             default:
                 break;
@@ -268,5 +377,3 @@ int main()
     
     return 0;
 }
-
-
